039c1a641344e077a209aebef8c92a02b8408bef
Bruno Sa <brunosantana.engenharia@gmail.com>
2024-02-13 18:08:33 -0300

    Feat/implementado os specifications de module e lesson

--- course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -7,6 +7,9 @@ import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.ModuleService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -40,14 +43,13 @@ public class ModuleServiceImpl implements ModuleService {
     public Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId) {
         return moduleRepository.findModuleIntoCourse(courseId,moduleId);
     }
-
-    @Override
-    public List<ModuleModel> findAllByCourse(UUID courseId) {
-        return moduleRepository.findAllModulesIntoCourse(courseId);
-    }
-
     @Override
     public Optional<ModuleModel> findById(UUID moduleId) {
         return moduleRepository.findById(moduleId);
+    }
+
+    @Override
+    public Page<ModuleModel> findAllByCourse(Specification<ModuleModel> spec, Pageable pageable) {
+        return moduleRepository.findAll(spec,pageable);
     }
 }
