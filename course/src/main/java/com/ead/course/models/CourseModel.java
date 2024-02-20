cf717303fd6662644183b9b3d0796b60208d6e9e
Bruno Sa <brunosantana.engenharia@gmail.com>
2024-02-20 13:32:35 -0300

    feat/mapeado as entidade e feito os relacionamentos, feito tambÃém a estrutura do service e repository

--- course/src/main/java/com/ead/course/models/CourseModel.java
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -9,8 +9,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -52,4 +50,9 @@ public class CourseModel implements Serializable {
     @Fetch(FetchMode.SUBSELECT)
     // @OnDelete(action = OnDeleteAction.CASCADE)
     private Set<ModuleModel> modules;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private Set<CourseUserModel> coursesUsers;
+
 }
